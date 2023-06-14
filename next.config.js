1af35df8991922657cc3d366c970f613edb4fcb5
sagar3595 <sagar47kumar13@gmail.com>
2023-06-14 23:11:43 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:[
+            "rcgkwikxhmnyeycdeaie.supabase.co"
+        ]
+    }
+}
+
+module.exports = nextConfig
